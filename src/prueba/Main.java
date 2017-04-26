66132933507cb0f6f1f396298fe80bfff635486b
kmphoz666 <kmphoz666@kmphoz666-PC>
2017-04-25 20:00:51 -0600

    cambio 2

--- src/prueba/Main.java
+++ src/prueba/Main.java
@@ -3,9 +3,8 @@ package prueba;
 public class Main
 {
 	public static void main(String[] args)
-	{
-	
-		System.out.println("LA PUTA Q TE PARIO");
+	{	
+		System.out.println("LA PUTA Q TE PARIO!");
 	}
 
 }
